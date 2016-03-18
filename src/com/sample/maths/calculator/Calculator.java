637d7ad3045c3a78de0b2327a16e01bdfdbbe413
szabbas <szabbas135@yahoo.com>
2016-03-18 12:01:31 +0530

    Included on more test case (sum of numbers)

--- src/com/sample/maths/calculator/Calculator.java
+++ src/com/sample/maths/calculator/Calculator.java
@@ -5,5 +5,10 @@ public class Calculator {
 	public int square(int num) {
 		return num*num;
 	}
+	
+	public int sumofnumbers(int a , int b){
+		return a+b;
+		
+	}
 
 }
--- test/com/sample/maths/calculator/CalculatorTest.java
+++ test/com/sample/maths/calculator/CalculatorTest.java
@@ -5,15 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculatorTest {
+	
+	Calculator calc=new Calculator();
+	
 	/*
 	 * Calculate squares of number like
 	 * 1- 1*1=1
 	 */
 	@Test
 	public void testSquares(){
-		Calculator calc=new Calculator();
 		assertEquals(calc.square(5),5*5);
 		assertEquals(calc.square(25),25*25);
+	}
+	
+	@Test
+	public void testSumOfNumbers(){
+		assertEquals(calc.sumofnumbers(10, 10), 10+10);
 	}
 	
 	
